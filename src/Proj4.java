1fc54a715512315885c7d1604db7e447a41cb26c
Heather DeMarco <hdemarco@msudenver.edu>
2017-10-24 23:23:50 -0600

    Initial commit

---
+++ src/Proj4.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Proj4 {
+
+
+    public static void main(String[] args){
+
+        System.out.print("How many values: ");
+        int v = new Scanner(System.in).nextInt();
+
+        System.out.print("Enter the values in order: ");
+        int l = new Scanner(System.in).nextInt();
+
+
+
+
+
+
+
+        System.out.print("Choose first or last (F or L): ");
+        int c = new Scanner(System.in).nextInt();
+
+
+    }
+
+
+
+
+
+
+}
